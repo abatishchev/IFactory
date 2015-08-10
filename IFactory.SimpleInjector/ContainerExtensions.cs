4a470e7c14e4e5983be11078bbc68beb5f80d8c2
Alexander Batishchev <abatishchev@gmail.com>
2015-08-09 20:09:58 -0700

    Adding IFactory.SimpleInjector

--- IFactory.SimpleInjector/ContainerExtensions.cs
+++ IFactory.SimpleInjector/ContainerExtensions.cs
@@ -1,28 +1,27 @@
-using Factory;
 using SimpleInjector;
 
 namespace Factory.SimpleInjector
 {
 	public static class ContainerExtensions
 	{
-		public static void RegisterFactory<T, TFactory>(this Container container)
+		public static void RegisterFactory<T, TFactory>(this Container container, Lifestyle instanceLifestyle = null, Lifestyle factoryLifestyle = null)
 			where T : class
 			where TFactory : class, IFactory<T>
 		{
-			container.Register<IFactory<T>, TFactory>(Lifestyle.Singleton);
-			container.Register(() => container.GetInstance<TFactory>().Create());
+			container.Register<IFactory<T>, TFactory>(factoryLifestyle ?? Lifestyle.Singleton);
+			container.Register(() => container.GetInstance<TFactory>().Create(), instanceLifestyle ?? Lifestyle.Transient);
 		}
 
-		public static void RegisterFactory<T, U, TFactory>(this Container container)
+		public static void RegisterFactory<T, U, TFactory>(this Container container, Lifestyle factoryLifestyle = null)
 			where TFactory : class, IFactory<T, U>
 		{
-			container.Register<IFactory<T, U>, TFactory>(Lifestyle.Singleton);
+			container.Register<IFactory<T, U>, TFactory>(factoryLifestyle ?? Lifestyle.Singleton);
 		}
 
-		public static void RegisterFactory<T, U1, U2, TFactory>(this Container container)
+		public static void RegisterFactory<T, U1, U2, TFactory>(this Container container, Lifestyle factoryLifestyle = null)
 			where TFactory : class, IFactory<T, U1, U2>
 		{
-			container.Register<IFactory<T, U1, U2>, TFactory>(Lifestyle.Singleton);
+			container.Register<IFactory<T, U1, U2>, TFactory>(factoryLifestyle ?? Lifestyle.Singleton);
 		}
 	}
 }
